dd737b73e3574cfe2fa6bebfb8c87205b9debe0c
iroess <iroess>
2002-07-14 02:28:30 +0000

    basic classes necessary for modelling preemption problems: - Lisa_nestedList  (--> class for modelling e.g. a pmtn-operation) - Lisa_VectorOfLists (--> class for modelling e.g. a set/vector of pmtn-operations) - Lisa_MatrixOfLists (--> class for modelling e.g. a matrix of pmtn-operations)

---
+++ src/basics/lmatrix.cpp
@@ -0,0 +1,456 @@
+/*
+ * ************** listmatrix.cpp *******************************
+ * 
+ * class listmatrix
+ *
+ * Owner: Ivo Roessling
+ *
+ * 11.05.01
+ */
+#include<ostream.h>
+#include"lisa_str.hpp"
+#include "pair.hpp"
+#include"lmatrix.hpp"
+extern string G_ExtraException;
+
+/* *******************************************************
+ * 
+ * begin of classdef for listvector
+ *
+ */
+
+
+// creating a VectorOfLists with size m  (--> combination of m Lisa_nestedLists)
+template<class T>
+Lisa_VectorOfLists<T>::Lisa_VectorOfLists(int m_in)
+     {
+       m=m_in;   //setting the internal size variable
+       contents= new Lisa_nestedList<T>[m];   //the contents: an array of Lisa_nestedList (=nestedList)
+     }  
+
+
+// copy-constructor
+template<class T>
+Lisa_VectorOfLists<T>::Lisa_VectorOfLists(const Lisa_VectorOfLists<T>& other){
+#ifdef DEBUG
+       G_ExceptionList.lthrow("(Warning) listvector given by value");
+#endif
+       m=other.m; 
+       
+       contents= new Lisa_nestedList<T>[m];
+       for (unsigned int i=0;i<m;i++) 
+	 contents[i] = other.contents[i];
+}
+
+
+template<class T>
+Lisa_VectorOfLists<T>::Lisa_VectorOfLists(const Lisa_Vector<T>& nonpreemtive) {
+#ifdef DEBUG
+       G_ExceptionList.lthrow("(Warning) listvector given by value");
+#endif
+       m=nonpreemtive.get_m(); 
+       
+       contents= new Lisa_nestedList<T>[m];
+       for (unsigned int i=0;i<m;i++) 
+	 contents[i].append(nonpreemtive[i]);
+}
+
+
+//*************************
+//iroess: noch zu implementieren:
+template<class T>
+void Lisa_VectorOfLists<T>::fill(T wert) 
+     {
+       unsigned int i;
+       for(i=m; i--; ) contents[i].fill(wert);
+     }
+//*************************/
+
+//*************************
+//iroess: noch zu implementieren:
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_max() const
+{
+  unsigned int i,j;
+  T value=contents[0].max();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->max()>value) { value=go->max(); i=j; }
+  return i;
+}
+
+/*
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_maxsum() const
+{
+  unsigned int i,j;
+  T value=contents[0].sum();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->sum()>value) { value=go->sum(); i=j; }
+  return i;
+}
+*/
+
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_maxlast() const
+{
+  unsigned int i,j;
+  T value=contents[0].last();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->last()>value) { value=go->last(); i=j; }
+  return i;
+}
+//*************************/
+
+
+//*************************
+//iroess: noch zu implementieren:
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_min() const 
+{
+  unsigned int i,j;
+  T value=contents[0].min();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->min()<value) { value=go->min(); i=j; }
+  return i;
+}
+
+/*
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_minsum() const 
+{
+  unsigned int i,j;
+  T value=contents[0].sum();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->sum()<value) { value=go->sum(); i=j; }
+  return i;
+}
+*/
+
+template<class T>
+int Lisa_VectorOfLists<T>::index_of_minlast() const 
+{
+  unsigned int i,j;
+  T value=contents[0].last();
+  Lisa_nestedList<T> * go;
+  i=0;
+  for(j=1,go=contents+1 ; j<m ; go++,j++) 
+    if (go->last()<value) { value=go->last(); i=j; }
+  return i;
+}
+//*************************/
+
+// assign-operator: assigning a VectorOfLists another one
+template<class T>
+const Lisa_VectorOfLists<T>& Lisa_VectorOfLists<T>::operator=(const Lisa_VectorOfLists<T>& other) 
+     {
+       if (&other != this) {
+	 m=other.m; 
+	 contents= new Lisa_nestedList<T>[m];
+	 for (unsigned int i=0;i<m;i++) {
+	   contents[i] = other.contents[i];
+	 }
+
+       }
+       return *this;
+     }  
+
+
+// to write object's defining data into an (output)stream
+template<class T>
+void Lisa_VectorOfLists<T>::write(ostream& strm) const 
+     {
+       unsigned int i;
+       strm << "{ ";
+       for (i=0 ; i<m ; i++)
+          { 
+	    //            strm.width(3);                        // witdh of next output
+            strm<<contents[i] << " ";
+          }
+       strm << "}\n";
+     }
+
+
+// to read object's defining data out of an (input)stream
+template<class T>
+void Lisa_VectorOfLists<T>::read(istream& strm) 
+     {
+       unsigned int i;
+       string S;
+  
+       S="";strm >> S; 
+       if (S!="{") 
+          G_ExceptionList.lthrow("{ expected in listvector.read",SYNTAX_ERROR);
+       for (i=0;i<m;i++)
+          strm >> contents[i];
+       S="";strm >> S; 
+       if (S!="}")
+          G_ExceptionList.lthrow("} expected in listvector.read",SYNTAX_ERROR);
+     }
+
+
+// the std. destructor
+template<class T>
+Lisa_VectorOfLists<T>::~Lisa_VectorOfLists()
+     {
+#ifdef DEBUG
+       if (!contents) G_ExceptionList.lthrow("listvector without contents");
+#endif       
+       delete[] contents;  // iroess: muss ich hier vorher explizit die einzelnen eintraege killen?!?
+     }
+
+
+//*************************
+//iroess: how(!) should i implement this relation?!
+// definition of equality-test operator
+template<class T>
+bool Lisa_VectorOfLists<T>::operator==(const Lisa_VectorOfLists<T>& other) const
+     { 
+       if (!(get_m()==other.get_m())) return false;
+       unsigned int i;
+       for (i=0; i<m; i++) 
+          if (!(contents[i]== other.contents[i])) return FALSE;
+       return true;
+     }
+//*************************/
+
+
+/*************************
+//iroess: how(!) should i implement this relation?!
+template<class T>
+bool Lisa_VectorOfLists<T>::operator<=(const Lisa_VectorOfLists<T>& other) const
+     { 
+       unsigned int i;
+       for (i=0; i<m; i++) 
+          {
+            if (contents[i]> other.contents[i]) return FALSE;
+            if (contents[i]< other.contents[i]) return TRUE;
+          }
+       return true;
+     }
+*************************/
+
+
+
+/* 
+ *
+ * end of classdef for listvector
+ *
+ * *******************************************************/
+
+
+
+/* *******************************************************
+ * 
+ * begin of classdef for listmatrix
+ *
+ */
+
+// copy-constructor
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(const Lisa_MatrixOfLists<T>& other)
+     {
+       int i;
+#ifdef DEBUG
+       G_ExceptionList.lthrow("(Warning) matrix given by value");
+#endif
+       n=other.n;
+       m=other.m;
+       row=new Lisa_VectorOfLists<T>[n](m);
+       for ( i=n ; i-- ; )
+          row[i]=other.row[i];
+     }
+
+// constructor for matrix with format (n)x(m) of list operations
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(int n_in, int m_in)
+     {
+       n=n_in; m=m_in;
+       row=new Lisa_VectorOfLists<T>[n](m);
+     }
+
+// copy-constructor
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(const Lisa_Matrix<T>& nonpreemptive) {
+#ifdef DEBUG
+       G_ExceptionList.lthrow("(Warning) listmatrix given by value");
+#endif
+       m=nonpreemptive.get_m(); 
+       n=nonpreemptive.get_n(); 
+       
+       row=new Lisa_VectorOfLists<T>[n](m);
+       for (unsigned int j=0; j<n;j++)
+	 for (unsigned int i=0; i<m;i++)
+	   row[j][i].append(nonpreemptive[j][i]);
+}
+
+
+
+template<class T>
+void Lisa_MatrixOfLists<T>::fill(const T wert) 
+     {
+       int i;
+       for ( i=n ; i-- ; )
+           row[i].fill(wert);
+     }
+
+// assign-operator: assigning a MatrixOfLists another one
+template<class T>
+const Lisa_MatrixOfLists<T>& Lisa_MatrixOfLists<T>::operator=(const Lisa_MatrixOfLists<T>& other) 
+{
+  Lisa_VectorOfLists<T> * rowptr;
+  Lisa_VectorOfLists<T> * otherptr;
+  Lisa_VectorOfLists<T> * endptr;
+
+  if (&other == this)
+    return *this;
+  if ((n!=other.n) || (m!=other.m))
+    {
+      G_ExceptionList.lthrow("wrong format argument to matrix.operator=");
+      return *this;
+    } 
+
+  endptr=end_row();
+  for (rowptr=first_row(), otherptr=other.first_row(); rowptr!=endptr; 
+       rowptr++, otherptr++)
+    *rowptr=*otherptr; 
+  return *this;
+}      
+
+
+template<class T>
+void Lisa_MatrixOfLists<T>::write(ostream& strm) const 
+     {
+       unsigned int i;
+       strm << "{\n";
+       for (i=0 ; i<n ; i++) 
+          strm << " " << row[i];
+       strm << "}\n";
+     }
+
+
+template<class T>
+void Lisa_MatrixOfLists<T>::read(istream& strm) 
+     {
+       unsigned int i;
+       string S;
+  
+       S="";strm >> S; 
+       if (S!="{")           
+         G_ExceptionList.lthrow("} expected in listmatrix.read",SYNTAX_ERROR);
+       for (i=0;i<n;i++)
+          {
+	    strm >> row[i];
+          }
+       S="";strm >> S; 
+       if (S!="}")           
+         G_ExceptionList.lthrow("} expected in listmatrix.read",SYNTAX_ERROR);
+     } 
+
+
+template<class T>
+Lisa_MatrixOfLists<T>::~Lisa_MatrixOfLists()
+     {
+       if (row) delete[] row;
+     }
+
+
+//*************************
+//iroess: noch zu implementieren:
+template<class T>
+bool Lisa_MatrixOfLists<T>::operator==(const Lisa_MatrixOfLists<T>& other) const
+     { 
+       if (!(get_m()==other.get_m() && get_n()==other.get_n())) return false;
+       unsigned int i;
+       for (i=0; i<n; i++) 
+          if (!(row[i]== other.row[i])) return FALSE;
+       return true;
+     }
+//*************************/
+
+template<class T>
+Lisa_Matrix<int> * Lisa_MatrixOfLists<T>::get_MatrixA() const {
+  Lisa_Matrix<int> *result=new Lisa_Matrix<int>(m,n);
+  for (unsigned int j=0; j<n;j++)
+    for (unsigned int i=0; i<m;i++)
+      (*result)[i][j]=row[j][i].length();
+  return result;
+}
+
+template<class T>
+void Lisa_MatrixOfLists<T>::apply_MatrixA(const Lisa_Matrix<int>& MatrixA, T NullElt ) {
+  for (unsigned int j=0; j<n;j++) 
+    for (unsigned int i=0; i<m;i++) {
+      for (;row[j][i].length()<MatrixA[j][i];row[j][i].dequeue());
+      for (;row[j][i].length()>MatrixA[j][i];row[j][i].append(NullElt));
+    }
+
+}
+
+/*************************
+//iroess: noch zu implementieren:
+template<class T>
+bool Lisa_MatrixOfLists<T>::operator<=(const Lisa_MatrixOfLists<T>& other) const
+     { 
+       unsigned int i, j;
+       for (i=0; i<n; i++)
+          for (j=0; j<m; j++) 
+            {
+              if (row[i][j]> other.row[i][j]) return FALSE;
+            if (row[i][j]< other.row[i][j]) return TRUE;
+          }
+       return true;
+     }
+*************************/
+
+/*
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(const int n_in, const int m_in) {
+    A=new Lisa_Matrix<int>(n_in,m_in);
+}
+
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(const Lisa_Matrix<int> other_A) {
+  *A=other_A;
+}
+
+template<class T>
+Lisa_MatrixOfLists<T>::Lisa_MatrixOfLists(Lisa_Matrix<int> *other_A) {
+  A=other_A;
+}
+
+*/
+
+// define classes for following types T:
+template class Lisa_nestedList<pair>;
+
+
+template class Lisa_MatrixOfLists<int>;
+template class Lisa_MatrixOfLists<float>;
+template class Lisa_MatrixOfLists<double>;
+template class Lisa_MatrixOfLists<bool>;
+template class Lisa_MatrixOfLists<pair>;
+
+
+template class Lisa_VectorOfLists<int>;
+template class Lisa_VectorOfLists<float>;
+template class Lisa_VectorOfLists<double>;
+template class Lisa_VectorOfLists<bool>;
+template class Lisa_VectorOfLists<string>;
+template class Lisa_VectorOfLists<pair>;
+
+
+//template class Lisa_MatrixOfLists<void *>;
+//template class Lisa_VectorOfLists<void *>;
+
+template class Lisa_MatrixOfLists<long>;
+template class Lisa_VectorOfLists<long>;
---
+++ src/basics/lmatrix.hpp
@@ -0,0 +1,275 @@
+/*
+ * ************** lmatrix.hpp *******************************
+ * 
+ * classes listvector & listmatrix
+ *
+ * Owner: Ivo Roessling
+ *
+ * 11.05.2001
+ * 
+ */
+
+#ifndef _lmatrix_h
+#define _lmatrix_h
+#include "global.hpp"
+#include "matrix.hpp"
+#include "list.hpp"
+
+
+
+/* *******************************************************
+ * 
+ * begin of classdef for pmtnoperation (nestedList)
+ *
+ */
+
+// Do not use the following type explicitely!
+template<class T>
+class Lisa_nestedList:public Lisa_List<T>
+{
+public:
+  void write(ostream& strm) const
+    {
+      Lisa_Node<T> * temp;
+      strm << " ( ";
+      temp=first_last->succ;
+      while (temp!=first_last) 
+	{ 
+	  strm << ((Lisa_Node<T>*)temp)->value << " "; 
+	  temp=temp->succ;
+	} while (temp!=first_last);
+      strm << ") ";
+    };
+
+  void fill(T wert) {
+    for (Lisa_Node<T> * temp=first_last->succ;
+	 temp!=first_last;
+	 temp=temp->succ) {
+      temp->value=wert;
+    }
+  }
+
+  /*
+    bool split_up(int x) {
+    if (length()<=0) return false;
+    if ((first_last->pred)->value < x) return false;
+    (first_last->pred)->value-=x;
+    append(x);
+    return true;
+  }
+  */
+
+};
+
+/* 
+ * 
+ * end of classdef for pmtnoperation
+ *
+ * *******************************************************/
+
+/* *******************************************************
+ * 
+ * begin of classdef for listvector
+ *
+ */
+
+// Do not use the following type explicitely!
+template<class T>
+class Lisa_VectorOfLists 
+{ 
+protected:
+  // size:
+  unsigned int m;
+  Lisa_nestedList<T>* contents;   
+public:
+
+  Lisa_VectorOfLists() {contents=NULL; cout<<"content set to null"<<endl;}
+  /// construct listvector with m elements of type T
+  Lisa_VectorOfLists(const int m);
+  /// construct listvector as a copy of other
+  Lisa_VectorOfLists(const Lisa_VectorOfLists<T>& other);
+  /// construct listvector out of a nonpremptive one
+  Lisa_VectorOfLists(const Lisa_Vector<T>& nonpreemtive);
+
+  /// assign value to all elements 
+  void fill(const T value);             
+  /// access to j-th element of vector 
+  Lisa_nestedList<T>& operator[] (const unsigned int j)   
+    { 
+      #ifdef DEBUG
+      if (j>=m) 
+	{
+	  G_ExceptionList.lthrow("element "+string((int)j)+" of a "+
+				 string((int)m)+"-element vector requested",
+				 OUT_OF_RANGE);
+	  return contents[0];
+	}
+      #endif
+      return contents[j];
+    }; 
+  Lisa_nestedList<T> operator[] (const unsigned int j) const   
+    { 
+      #ifdef DEBUG
+      if (j>=m) 
+	{
+	  G_ExceptionList.lthrow("element "+string((int)j)+" of a "+
+				 string((int)m)+"-element vector requested",
+				 OUT_OF_RANGE);
+	  return contents[0];
+	}
+      #endif
+      return contents[j];
+    }; 
+  /// length of vector
+  int get_m() const { return m; }
+  /// assign a vector to another  
+  const Lisa_VectorOfLists<T>& operator=(const Lisa_VectorOfLists<T>& );
+  /// compare vectors
+  bool operator==(const Lisa_VectorOfLists<T>&) const;
+  bool operator!=(const Lisa_VectorOfLists<T>& other) const { return !(&other == this); }
+  /// compare lexicographically
+  bool operator<=(const Lisa_VectorOfLists<T>&) const;
+  // input/output in lisa-format
+  void write(ostream& = cout) const;
+  void read(istream& = cin);
+  /// search index of maximal vector element
+  int Lisa_VectorOfLists<T>::index_of_max() const;
+  int Lisa_VectorOfLists<T>::index_of_maxsum() const;
+  int Lisa_VectorOfLists<T>::index_of_maxlast() const;
+  /// search index of minimal vector element
+  int Lisa_VectorOfLists<T>::index_of_min() const;
+  int Lisa_VectorOfLists<T>::index_of_minsum() const;
+  int Lisa_VectorOfLists<T>::index_of_minlast() const;
+  ~Lisa_VectorOfLists();
+  Lisa_nestedList<T>* get_first() const { return contents; }
+  Lisa_nestedList<T>* get_last() const { return contents+m; }
+};
+
+template<class T>
+inline ostream& operator << (ostream&strm, const Lisa_VectorOfLists<T>& l)
+{
+  l.write(strm);
+  return strm;
+};
+
+template<class T>
+inline istream& operator >> (istream&strm, Lisa_VectorOfLists<T>& l)
+{
+  l.read(strm);
+  return strm;
+};
+
+
+
+/* 
+ * 
+ * end of classdef for listvector
+ *
+ * *******************************************************/
+
+
+/* *******************************************************
+ * 
+ * begin of classdef for listmatrix
+ *
+ */
+
+
+// Do not use the following type explicitely!
+
+template<class T>
+class Lisa_MatrixOfLists: public Lisa_Object 
+{
+protected:
+  unsigned int n, m;
+  Lisa_VectorOfLists<T>* row;
+  Lisa_Matrix<int> *A;
+
+public:
+  /// construct list matrix of given format n x m
+  Lisa_MatrixOfLists(const int n_in, const int m_in);
+  /// construct copy of a given pmt matrix
+  Lisa_MatrixOfLists(const Lisa_MatrixOfLists<T>& other);
+  /// construct listmatrix out of a nonpremptive one
+  Lisa_MatrixOfLists(const Lisa_Matrix<T>& nonpreemptive);
+  /// assign value to all matrix elements
+  void fill(const T value);
+  /// access to i-th row vector
+  Lisa_VectorOfLists<T>& operator[] (const unsigned i)  
+    { 
+      #ifdef DEBUG
+      if (i>=n)
+	{
+	  G_ExceptionList.lthrow("row "+string((int)i)+" of a "+
+				 string((int)n)+"-row listmatrix requested",
+				 OUT_OF_RANGE);
+	  return row[0];
+	}
+      #endif
+      return row[i];
+    }; 
+  const Lisa_VectorOfLists<T>& operator[] (const unsigned i) const  
+    { 
+      #ifdef DEBUG
+      if (i>=n)
+	{
+	  G_ExceptionList.lthrow("row "+string((int)i)+" of a "+
+				 string((int)n)+"-row listmatrix requested",
+				 OUT_OF_RANGE);
+	  return row[0];
+	}
+      #endif
+      return row[i];
+    };  
+  /// number of columns
+  int get_m() const { return m; }
+  /// number of rows           
+  int get_n() const { return n; } 
+  /// assign another matrix 
+  const Lisa_MatrixOfLists<T>& operator=(const Lisa_MatrixOfLists<T>& other);
+  /// compare matrices element by element
+  bool operator==(const Lisa_MatrixOfLists<T>&) const;
+  
+  /// compare lexicographically
+  //iroess  bool operator<=(const Lisa_MatrixOfLists<T>&) const;
+  // input/output in LiSA-format:
+  void write(ostream& = cout) const;
+  void read(istream& = cin);  
+  ~Lisa_MatrixOfLists();
+  /// pointer to first row (for iterating the contents)
+  Lisa_VectorOfLists<T> * first_row() const { return row; }
+  /// Pointer after last row (for iterating the contents)
+  Lisa_VectorOfLists<T> * end_row() const { return row+n; }
+
+  Lisa_Matrix<int> * get_MatrixA() const;
+  void apply_MatrixA(const Lisa_Matrix<int>& MatrixA, T NullElt);
+  /*
+public:
+  Lisa_MatrixOfLists(const Lisa_MatrixOfLists<T> & other);
+  Lisa_MatrixOfLists(const int n_in, const int m_in);
+  Lisa_MatrixOfLists(const Lisa_Matrix<int> other_A);
+  Lisa_MatrixOfLists(Lisa_Matrix<int> *other_A);
+
+  void read(istream & = cin) {}
+  void write(ostream & = cout) const{}
+  */
+};
+
+
+template<class T>
+inline ostream& operator << (ostream&strm, const Lisa_MatrixOfLists<T>& l)
+{
+  l.write(strm);
+  return strm;
+};
+
+template<class T>
+inline istream& operator >> (istream&strm, Lisa_MatrixOfLists<T>& l)
+{
+  l.read(strm);
+  return strm;
+};
+
+
+#endif             
+
