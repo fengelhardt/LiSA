06806986030bd4229bb01442fe32e60ecf87f253
moerig <moerig>
2003-03-06 17:08:26 +0000

    changed the tcl hack a bit

--- src/desktop/setreadv.hpp
+++ src/desktop/setreadv.hpp
@@ -34,13 +34,17 @@
 //tcl version hack
 #if TCL_MAJOR_VERSION < 8
  #define TCL_HACK_CHAR char
-#elseif TCL_MAJOR_VERSION == 8 
+#endif
+
+#if TCL_MAJOR_VERSION == 8 
  #if TCL_MINOR_VERSION < 4
   #define TCL_HACK_CHAR char
  #else
   #define TCL_HACK_CHAR const char
  #endif
-#else
+#endif
+
+#if TCL_MAJOR_VERSION > 8
  #define TCL_HACK_CHAR const char
 #endif
 
