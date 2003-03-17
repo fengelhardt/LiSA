005527f13b7c3ccade847b62262d559fcd2f2ad8
moerig <moerig>
2003-03-17 13:22:52 +0000

    killed an unnessecary file, re-enabled bruckers algorithm for flow shop problems

--- src/algorithm/js_bb_br/js_bb_br.cpp
+++ src/algorithm/js_bb_br/js_bb_br.cpp
@@ -7,8 +7,37 @@
 
 
 #include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
 
-#include "js_bb_br.hpp"
+#include <iostream>
+#include <fstream>
+
+#include "../../basics/matrix.hpp"
+#include "../../scheduling/js_sched.hpp"
+#include "../../lisa/ptype.hpp"
+#include "../../scheduling/schedule.hpp"
+#include "../../lisa/lvalues.hpp"
+#include "../../lisa/ctrlpara.hpp"
+#include "../../misc/except.hpp"
+
+#include "jb_block.hpp"
+#include "jb_brnch.hpp"
+#include "jb_dtstr.hpp"
+#include "jb_fix.hpp"
+#include "jb_hd_tl.hpp"
+#include "jb_heur.hpp"
+#include "jb_init.hpp"
+#include "jb_list.hpp"
+#include "jb_low.hpp"
+#include "jb_rd_dt.hpp"
+#include "jb_selct.hpp"
+#include "jb_sort.hpp"
+#include "jb_stack.hpp"
+#include "jb_table.hpp"
 
 using namespace std;
 
