e6a28ae00a423edae72249c1034263727931e583
moerig <moerig>
2003-03-25 12:46:20 +0000

    sortet io methods a bit

--- src/main/file_io.cpp
+++ src/main/file_io.cpp
@@ -30,120 +30,115 @@ extern class Lisa_XSchedule * G_XSchedule;
 int read(string dname) {
   int oldm=G_Values.get_m();
   int oldn=G_Values.get_n();
+  
   ifstream fin(dname.c_str());
   if (fin==NULL) {
-    G_ExceptionList.lthrow("cannot open file: "+dname+"\n",END_OF_FILE);
+    G_ExceptionList.lthrow("Cannot open file '"+dname+"' for reading.",END_OF_FILE);
     return 1;
-  }  
+  }
+  
+  /// check what is in inputfile
   string S;
   bool fvalues=FALSE,fproblemtype=FALSE;
   int no_schedules=0;
-  for (;;)
-    {
-      S=""; 
-      fin >>S;
-      if (S=="") 
-	{ 
-	  break;
-	} 
-      if (S=="<SCHEDULE>") no_schedules++;
-      if (S=="<PROBLEMTYPE>") fproblemtype=TRUE;
-      if (S=="<VALUES>") fvalues=TRUE ;
-    }
-
+  for (;;){
+    S=""; 
+    fin >> S;
+    if (S=="") break;
+    
+    if (S=="<SCHEDULE>") no_schedules++;
+    if (S=="<PROBLEMTYPE>") fproblemtype=TRUE;
+    if (S=="<VALUES>") fvalues=TRUE ;
+  }
+  
+  // read problemtype
   if (fproblemtype) {
     ifstream fin(dname.c_str());
     if (fin==NULL) {
-      G_ExceptionList.lthrow("cannot open file: "+dname,END_OF_FILE);
+      G_ExceptionList.lthrow("Cannot open file '"+dname+"' for reading.",END_OF_FILE);
       return 1;
     }
+    
     fin >> G_ProblemType;
   } else {
-     G_ExceptionList.lthrow("no Problem defined in: "+dname,END_OF_FILE);
+    G_ExceptionList.lthrow("No problem defined in file '"+dname+"'.",END_OF_FILE);
     return 1;
   }
+  
+  // read problem instance (values)
   if (fvalues) {
     ifstream fin(dname.c_str());
     if (fin==NULL) {
-      G_ExceptionList.lthrow("cannot open file: "+dname,END_OF_FILE);
+      G_ExceptionList.lthrow("Cannot open file '"+dname+"' for reading.",END_OF_FILE);
       return 1;
     }
+    
     fin >> G_Values;
-    if(oldm!=G_Values.get_m()||oldn!=G_Values.get_n())
-      new_mn(); 
+    if(oldm!=G_Values.get_m()||oldn!=G_Values.get_n()) new_mn(); 
     new_problemtype();
-    if(oldm!=G_Values.get_m()||oldn!=G_Values.get_n())
-      new_mn();
+    if(oldm!=G_Values.get_m()||oldn!=G_Values.get_n()) new_mn();
     new_values();
   } else {
     new_problemtype();
     return 0;
   }
+  
+  /// read values
   if (no_schedules) {
     ifstream fin(dname.c_str());
     if (fin==NULL) {
-      G_ExceptionList.lthrow("cannot open file: "+dname,END_OF_FILE);
+      G_ExceptionList.lthrow("Cannot open file '"+dname+"' for reading.",END_OF_FILE);
       return 1;
     }
-
+    
     Lisa_Schedule *mySchedule = 0;
     Lisa_ScheduleNode *myLisa_ScheduleNode;
-    int i;
-    //G_ScheduleList->clear();
-    while(!(G_ScheduleList->empty())) 
-	{
-          Lisa_ScheduleNode dummynode;
-          dummynode=G_ScheduleList->pop();
-          if (dummynode.actual_schedule!=G_Schedule) delete dummynode.actual_schedule;
-	}
-
-    if (no_schedules*G_Values.get_m()*G_Values.get_n()>MAX_MEMORY) {
-      G_ExceptionList.lthrow("to much schedules to read",WARNING);
-      int my_schedules=(int) MAX_MEMORY / (G_Values.get_m()*G_Values.get_n());
-      int actual_number=0;
-      for(i=1;i<=no_schedules && actual_number<my_schedules;i++) {
-	mySchedule = new Lisa_Schedule(1,1);
-	fin >> (*mySchedule);
-	if (mySchedule->get_property(NO_OF_SINKS) <=1) {
-	  myLisa_ScheduleNode= new Lisa_ScheduleNode(mySchedule);
-	  G_ScheduleList->append(*myLisa_ScheduleNode);
-	  actual_number++;
-	}
-      }
-      cout << actual_number << " schedules read, (maximal " << my_schedules << ")" << endl;
-    } else {
-      for(i=1;i<=no_schedules;i++) {
-	mySchedule = new Lisa_Schedule(1,1);
-	fin >> (*mySchedule);
-	//      cout << "myschedule" << *mySchedule << endl;
-	myLisa_ScheduleNode= new Lisa_ScheduleNode(mySchedule);
-	G_ScheduleList->append(*myLisa_ScheduleNode);
-      }
+    // clear old schedules in list
+    while(!(G_ScheduleList->empty())){
+      Lisa_ScheduleNode dummynode;
+      dummynode=G_ScheduleList->pop();
+      if (dummynode.actual_schedule!=G_Schedule) delete dummynode.actual_schedule;
     }
-      G_Schedule=mySchedule;
-      delete G_XSchedule;
-      G_XSchedule = new Lisa_XSchedule(G_Schedule);
-      new_schedule();
-    } else return 0;
+    
+    // read new schedules
+    for(int i=1;i<=no_schedules;i++) {
+      mySchedule = new Lisa_Schedule(1,1);
+      fin >> (*mySchedule);
+      myLisa_ScheduleNode= new Lisa_ScheduleNode(mySchedule);
+      G_ScheduleList->append(*myLisa_ScheduleNode);
+    }
+    
+    // make last schedule read the current schedule
+    G_Schedule=mySchedule;
+    delete G_XSchedule;
+    G_XSchedule = new Lisa_XSchedule(G_Schedule);
+    new_schedule();
+  }
+  
+  fin.close();
   return 0;
 }
 
 //**************************************************************************
 
 int save(string dname) {
+  
   ofstream fout(dname.c_str());
   if (fout==NULL) {
-    G_ExceptionList.lthrow("cannot write file: "+dname+"\n",END_OF_FILE);
+    G_ExceptionList.lthrow("Cannot open file: '"+dname+"' for writing.",END_OF_FILE);
     return 1;
   }
+  
   if (G_ProblemType.valid()) {
     fout << G_ProblemType;
     if (G_Values.valid ) {
       fout << G_Values;
       if (G_Schedule->valid) 
-	fout << *G_Schedule;
+        fout << *G_Schedule;
     }
   } 
+  
+  fout.close();
   return 0;
 }
 
