db5c03d3b69de983ba3fc3276dd9a70324868928
moerig <moerig>
2004-01-23 12:17:00 +0000

    changed algorithm IO to xml style

--- src/algorithm/bb/bb_main.cpp
+++ src/algorithm/bb/bb_main.cpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <iostream>
 
+#include "../../xml/LisaXmlFile.hpp"
+
 #include "../../basics/list.hpp"
 #include "../../main/global.hpp"
 #include "../../lisa/ctrlpara.hpp"
@@ -42,29 +44,72 @@ int main(int argc, char *argv[])
    cout << "PID= " << getpid() << endl; 
    ifstream i_strm(argv[1]);
    ofstream o_strm(argv[2]);
-   if (!i_strm)
-     {
-       cout << "ERROR: bb cannot find input file " << argv[1] << "." << endl;
-       exit(1);
-     }
+   if (!i_strm){
+     cout << "ERROR: bb cannot find input file " << argv[1] << "." << endl;
+     exit(1);
+   }
+   if (!o_strm){
+     cout << "ERROR: bb cannot open output file " << argv[2] << "." << endl;
+     exit(1);
+   }
+   i_strm.close();
+   o_strm.close();
    
-
-   // read problem description and decide whether program is applicable:
-   Lisa_ProblemType * lpr = new Lisa_ProblemType;
-   i_strm >> (*lpr);
-   if (!G_ExceptionList.empty())
-     {
-       cout << "ERROR: bb cannot read problem type, aborting program." << endl;
-       exit(1);
-     }  
+    //initialize class
+    LisaXmlFile::initialize();
+    //create Input handler
+    LisaXmlFile xmlInput(LisaXmlFile::IMPLICIT);
+    //communication objects
+    Lisa_ProblemType lpr;
+    Lisa_ControlParameters sp;   
+    Lisa_Values my_values;
+    
+    //parse xml file
+    xmlInput.read(argv[1]);
+    //determine document type
+    LisaXmlFile::DOC_TYPE type = xmlInput.getDocumentType();
+    
+    //check for successful parsing and valid document type
+    if (!xmlInput || !(type == LisaXmlFile::INSTANCE || type == LisaXmlFile::SOLUTION))
+    {
+      cout << "ERROR: cannot read input , aborting program." << endl;
+      exit(1);
+    }
+    //get Problem
+    if( !(xmlInput >> lpr))
+    {
+      cout << "ERROR: cannot read ProblemType , aborting program." << endl;
+      exit(1);
+    }
+    //get ControlParameters
+    if( !(xmlInput >> sp))
+    {
+      cout << "ERROR: cannot read ControlParameters , aborting program." << endl;
+      exit(1);
+    }
+    //get Values
+    if( !(xmlInput >> my_values))
+    {
+      cout << "ERROR: cannot read Values , aborting program." << endl;
+      exit(1);
+    }
+    // if something else went wrong
+    if (!G_ExceptionList.empty())
+    {
+      cout << "ERROR: cannot read input , aborting program." << endl;
+      exit(1);
+    }
+    
+  
+   // problem type
    int zfn, problem_type;
-   problem_type=lpr->get_property(M_ENV);
+   problem_type=lpr.get_property(M_ENV);
    string cannot_handle="";
-   if (lpr->get_property(PMTN)) cannot_handle="preemption";
-   if (lpr->get_property(PRECEDENCE)!=EMPTY) 
+   if (lpr.get_property(PMTN)) cannot_handle="preemption";
+   if (lpr.get_property(PRECEDENCE)!=EMPTY) 
      cannot_handle="precedence constraints"; 
-   if (lpr->get_property(BATCH))  cannot_handle="batching"; 
-   if (lpr->get_property(NO_WAIT))  cannot_handle="no-wait constraints";
+   if (lpr.get_property(BATCH))  cannot_handle="batching"; 
+   if (lpr.get_property(NO_WAIT))  cannot_handle="no-wait constraints";
    if (cannot_handle!="")  
       {
         cout << "ERROR: bb cannot handle " << cannot_handle << 
@@ -82,43 +127,39 @@ int main(int argc, char *argv[])
       default: G_ExceptionList.lthrow("unknown problem type, aborting");
         exit(1);
       }
-   zfn=lpr->get_property(OBJECTIVE);
+   zfn=lpr.get_property(OBJECTIVE);
    if ((zfn==IRREG1)||(zfn==IRREG2))
      {
        cout << "ERROR: bb cannot handle irregular objective functions. " << 
 	 "Aborting program." << endl;
        exit(1);
      }
-   delete lpr;
+
    
-   // read control parameters: 
-   Lisa_ControlParameters * sp = new Lisa_ControlParameters;
-   i_strm >> (*sp);
-   if (sp->defined("INS_ORDER")==Lisa_ControlParameters::STRING)
+   // control parameters: 
+   if (sp.defined("INS_ORDER")==Lisa_ControlParameters::STRING)
      {
-       if (sp->get_string("INS_ORDER")=="LPT") 
+       if (sp.get_string("INS_ORDER")=="LPT") 
 	 my_bb->set_ins_order(LPT_ORDER);
      }
-   if (sp->defined("NB_SOLUTIONS")==Lisa_ControlParameters::LONG)
-      my_bb->set_output_bound(sp->get_long("NB_SOLUTIONS"));
-   if (sp->defined("UPPER_BOUND")==Lisa_ControlParameters::DOUBLE)
-      my_bb->set_upper_bound((TIMETYP) sp->get_double("UPPER_BOUND"));
-   if (sp->defined("LOWER_BOUND")==Lisa_ControlParameters::DOUBLE)
-      my_bb->set_lower_bound((TIMETYP) sp->get_double("LOWER_BOUND"));
-   delete sp;
-      
-   // read problem instance and cast it into a Lisa_ShopProblem:
-   Lisa_Values * my_werte=new Lisa_Values;
-   i_strm >> (*my_werte);
+   if (sp.defined("NB_SOLUTIONS")==Lisa_ControlParameters::LONG)
+      my_bb->set_output_bound(sp.get_long("NB_SOLUTIONS"));
+   if (sp.defined("UPPER_BOUND")==Lisa_ControlParameters::DOUBLE)
+      my_bb->set_upper_bound((TIMETYP) sp.get_double("UPPER_BOUND"));
+   if (sp.defined("LOWER_BOUND")==Lisa_ControlParameters::DOUBLE)
+      my_bb->set_lower_bound((TIMETYP) sp.get_double("LOWER_BOUND"));
+  
+  
+   // handle problem instance and cast it into a Lisa_ShopProblem:
    Lisa_OsProblem * my_os_problem=0;
    Lisa_JsProblem * my_js_problem=0;
    switch (problem_type)
       {
-      case O: my_os_problem=new Lisa_OsProblem(my_werte);
+      case O: my_os_problem=new Lisa_OsProblem(&my_values);
 	break;
       case ONE:
       case F:
-      case J: my_js_problem=new Lisa_JsProblem(my_werte);
+      case J: my_js_problem=new Lisa_JsProblem(&my_values);
 	break;
       }
    if ((problem_type==F)||(problem_type==ONE))
@@ -128,9 +169,8 @@ int main(int argc, char *argv[])
        cout << "ERROR: No valid input in bb, aborting program." << endl;
        exit(1);
      }
-   int n=my_werte->get_n(); 
-   int m=my_werte->get_m();
-   delete my_werte;
+   int n=my_values.get_n(); 
+   int m=my_values.get_m();
 
    // solve the problem and store results in a list:
    Lisa_List<Lisa_Matrix<int> *>* results = new Lisa_List<Lisa_Matrix<int> *>;
@@ -150,17 +190,25 @@ int main(int argc, char *argv[])
    Lisa_Matrix<int> * outputlr;
    Lisa_Schedule * out_schedule = new Lisa_Schedule;
    out_schedule->init(n, m);
+   
    if (results->empty())
      G_ExceptionList.lthrow("no schedules found (maybe upper bound wrong?)");
-   while (!results->empty())
-        { 
-          outputlr= results->pop();
-          out_schedule->LR = outputlr;
-          o_strm << *out_schedule;
-	  delete outputlr; out_schedule->LR=NULL;
-        } 
+   
+   Lisa_List<Lisa_ScheduleNode> SchedList;
+   
+   while (!results->empty()){ 
+     outputlr= results->pop();
+     out_schedule->LR = outputlr;
+     SchedList.append(Lisa_ScheduleNode(out_schedule));
+     delete outputlr;
+     out_schedule->LR=NULL;
+   } 
    delete out_schedule;
-   delete results; 
+   delete results;
+   
+   LisaXmlFile xmlOutput(LisaXmlFile::SOLUTION);
+   xmlOutput << lpr << my_values << sp << SchedList;
+   xmlOutput.write(argv[2]);
 
    // end of program:  
    cout << "\nProgram finished after " << my_bb->get_nr_insert()
