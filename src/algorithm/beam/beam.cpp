8e4f2dfa832bef009151938e919a76b388ea6b3c
tusch <tusch>
2004-02-10 17:55:01 +0000

    fixed bug in ECT Ordering

--- src/algorithm/beam/beam.cpp
+++ src/algorithm/beam/beam.cpp
@@ -358,6 +358,7 @@ Lisa_Order* makeOrder(InsertionOrder iord, int& ops, Lisa_OsProblem *os_problem)
 										//non-existing ops get maximum key : +infinity;
 										l--;
 										order->read_one_key( i, j, DOUBLE_INF);
+										continue;
 								}
       if (iord == lpt) 
 								order->read_one_key( i, j, -(*(os_problem->time))[i+1][j+1]);
@@ -403,23 +404,34 @@ Lisa_Order* makeECT(int& ops, Lisa_OsProblem *os_problem){
 		for(int op = 0; op < l; op++)
 				{
 						//next operation found - insert in final order
-						int i = ECT->row(0), j = ECT->col(0);
-						order->read_one_key(i,j,op);
-						//mark current op as processed
-						compl_times[i + j*m] = -1.0;
-						//update completion times on machine j
-						for(int t = 0; t < n; t++)
+      int i = ECT->row(0), j = ECT->col(0);
+      order->read_one_key(i,j,op);
+      //mark current op as processed
+      compl_times[i + j*m] = -1.0;
+      //update completion times on machine j
+      for(int t = 0; t < n; t++)
 								{
 										//put processed to the end
 										if(compl_times[t + j*m] < 0.0)
-												ECT->read_one_key(i,j,DOUBLE_INF);
+												ECT->read_one_key(t,j,DOUBLE_INF);
 										//existing ops on machine j finish later now
-										else if ((*os_problem->sij)[i+1][j+1]){
+										else if ((*os_problem->sij)[t+1][j+1]){
 												compl_times[t + j*m] += (*os_problem->time)[i+1][j+1];
 												ECT->read_one_key(t,j,compl_times[t + j*m]);
 										}
 								}
-						ECT->sort();
+						//update completion times for task i
+      for(int k = 0; k < m; k++)
+								{
+										//put processed to the end
+										if(compl_times[i + k*m] < 0.0)
+												ECT->read_one_key(i,k,DOUBLE_INF);
+										else if ((*os_problem->sij)[i+1][k+1]){
+												compl_times[i + k*m] += (*os_problem->time)[i+1][j+1];
+												ECT->read_one_key(i,k,compl_times[i + k*m]);
+										}
+								}
+      ECT->sort();
 				}
 		delete[] compl_times;
 		delete ECT;
