11e034681ac63054b7b39cd021b2757cc1384b18
moerig <moerig>
2004-02-12 11:06:33 +0000

    changed algorithm io to xml style

--- src/algorithm/irred/irred.cpp
+++ src/algorithm/irred/irred.cpp
@@ -10,12 +10,15 @@
 #include <fstream>
 
 #include "../../main/global.hpp"
+#include "../../basics/list.hpp"
 #include "../../lisa/ctrlpara.hpp"
 #include "../../scheduling/schedule.hpp"
 #include "../../lisa/ptype.hpp"
 #include "../../scheduling/mo_jo.hpp"
 #include "../../lisa/lvalues.hpp"
 #include "../../misc/except.hpp"
+
+#include "../../xml/LisaXmlFile.hpp"
 
 #include "convertgraph.hpp"
 #include "irrednode.hpp"
@@ -25,39 +28,77 @@ using namespace std;
 
 //**************************************************************************
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 
-  G_ExceptionList.set_output_to_cout();   
+  G_ExceptionList.set_output_to_cout();
+  cout << "PID= " << getpid() << endl; 
+  
   // open files and assure existence:
   if (argc != 3) {
     cout << "\nUsage: " << argv[0] << " [input file] [output file]\n";
     exit(1);
   }
-  
-  cout << "PID= " << getpid() << endl; 
 
   ifstream i_strm(argv[1]);
   ofstream o_strm(argv[2]);
+  if (!i_strm){
+    cout << "ERROR: cannot open file " << argv[1] << " for reading." << endl;
+    exit(1);
+  }
+  if(!o_strm){
+    cout << "ERROR: cannot open file " << argv[2] << " for writing." << endl;
+    exit(1);
+  }
+  i_strm.close();
+  o_strm.close();
   
-  if (!i_strm) {
-    cout << "ERROR: cannot find input file " << argv[1] << "." << endl;
+  Lisa_ProblemType lpr;
+  Lisa_ControlParameters sp;
+  Lisa_Values my_werte; 
+  Lisa_Schedule my_schedule; 
+  
+  //initialize class
+  LisaXmlFile::initialize();
+  //create Input handler
+  LisaXmlFile xmlInput(LisaXmlFile::IMPLICIT);
+    
+  //parse xml file
+  xmlInput.read(argv[1]);
+  //determine document type
+  LisaXmlFile::DOC_TYPE type = xmlInput.getDocumentType();
+     
+  //check for successful parsing and valid document type
+  if (!xmlInput || !type == LisaXmlFile::SOLUTION){
+    cout << "ERROR: cannot read input, aborting program." << endl;
+    exit(1); 
+  }
+  //get Problem
+  if( !(xmlInput >> lpr)){
+    cout << "ERROR: cannot read ProblemType, aborting program." << endl;
+    exit(1);
+  }
+  //get ControlParameters
+  if( !(xmlInput >> sp)){
+    cout << "ERROR: cannot read ControlParameters, aborting program." << endl;
+    exit(1);
+  }
+  //get Values
+  if( !(xmlInput >> my_werte)){
+    cout << "ERROR: cannot read Values, aborting program." << endl;
+    exit(1);
+  }
+  if( !(xmlInput >> my_schedule)){
+    cout << "ERROR: cannot read Schedule, aborting program." << endl;
+    exit(1);
+  }
+  // if something else went wrong
+  if (!G_ExceptionList.empty()){
+    cout << "ERROR: cannot read input, aborting program." << endl;
     exit(1);
   }
   
-  // read problem description and decide whether program is applicable:
-  Lisa_ProblemType lpr;
-  i_strm >> lpr;
-  if (!G_ExceptionList.empty()){
-    cout << "ERROR: cannot read problem type, aborting program." << endl;
-    exit(1);
-  }  
-   
-  
-  // read control parameters: 
+  // parse control parameters: 
   int alg_param = Lisa_IrreducibilityTest::JUST_TEST;
-  Lisa_ControlParameters sp;
-  i_strm >> sp;
   if (sp.defined("TYPE")==Lisa_ControlParameters::STRING){
     if (sp.get_string("TYPE") == "ALL_REDUCING"){
       alg_param = Lisa_IrreducibilityTest::GENERATE_ALL;
@@ -91,12 +132,9 @@ int main(int argc, char *argv[])
     }
   }else cout << "WARNING: Could not read RNDM parameter, using default."<<endl;
  
-  // read problem instance
-  Lisa_Values my_werte;
-  i_strm >> my_werte;
-  
-  Lisa_Schedule my_schedule;
-  i_strm >> my_schedule;
+
+
+
   delete my_schedule.CIJ;
   my_schedule.CIJ = 0;
 
@@ -108,14 +146,6 @@ int main(int argc, char *argv[])
   int v = (*convert->get_disjkt()).get_vertices();
   Lisa_MatrixGraph pg(v);
   convert->plan2graph(my_schedule.LR,&pg);
-  
-  /*
-  cout << *my_schedule.LR;
-  cout << *convert->get_disjkt();
-  cout << pg;
-  convert->graph2plan(&pg,my_schedule.LR);
-  cout << *my_schedule.LR;
-  */
   
   Lisa_IrredResult* res;
   if (alg_param==Lisa_IrreducibilityTest::GENERATE_SIMILAR){ 
@@ -129,10 +159,12 @@ int main(int argc, char *argv[])
   Lisa_IrreducibilityTest my_test(convert->get_disjkt());
   my_test.set_output_to(res);
   bool irre = my_test.test(&pg,alg_param);
-  
+
+  Lisa_List<Lisa_ScheduleNode> SchedList;  
+    
   if (irre && alg_param!=Lisa_IrreducibilityTest::GENERATE_SIMILAR){
     cout << "WARNING: Plan is irreducible !" << endl;
-    o_strm << my_schedule;
+    SchedList.append(Lisa_ScheduleNode(&my_schedule));
   }else{
     if(alg_param==Lisa_IrreducibilityTest::JUST_TEST ||
        alg_param==Lisa_IrreducibilityTest::JUST_TEST_RANDOM){
@@ -162,23 +194,27 @@ int main(int argc, char *argv[])
     
     if(alg_param==Lisa_IrreducibilityTest::GENERATE_SIMILAR){ 
       if(irre) cout << "WARNING: Plan is irreducible !" << endl;
-      o_strm << my_schedule;
+      SchedList.append(Lisa_ScheduleNode(&my_schedule));
     }
     
     res->results->reset();
     do{
       if(res->results->get()->status == Lisa_IrredNode::IRREDUCIBLE){
         convert->graph2plan(res->results->get()->plangraph,my_schedule.LR);
-        o_strm << my_schedule;
+        SchedList.append(Lisa_ScheduleNode(&my_schedule));
       }else if(return_all){
         convert->graph2plan(res->results->get()->plangraph,my_schedule.LR);
-        o_strm << my_schedule;
+        SchedList.append(Lisa_ScheduleNode(&my_schedule));
       }
     }while(res->results->next());
     //  cout << "WARNING: Plan is NOT irreducible ! " << count 
     //       << " irreducible plan(s) found." <<endl;
   }
- 
+  
+  LisaXmlFile xmlOutput(LisaXmlFile::SOLUTION);
+  xmlOutput << lpr << my_werte << sp << SchedList;
+  xmlOutput.write(argv[2]);
+   
   delete convert;
   delete res;
   
