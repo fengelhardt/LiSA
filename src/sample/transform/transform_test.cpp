e654361aa50f5c1380f08e872c761047da2355d5
tusch <tusch>
2004-02-24 19:51:26 +0000

    improved output

--- src/sample/transform/transform_test.cpp
+++ src/sample/transform/transform_test.cpp
@@ -25,8 +25,10 @@ void usage(int argc, char** argv){
 
 bool read(const char* f1,
 										const char* f2,
-										Lisa_ProblemType& P1, 
-										Lisa_ProblemType& P2, 
+										Lisa_ProblemType& r_P1, 
+										Lisa_ProblemType& r_P2,
+										Lisa_ProblemType& m_P1, 
+										Lisa_ProblemType& m_P2,
 										Lisa_Values& V1)
 {
 		//initialize class
@@ -34,6 +36,8 @@ bool read(const char* f1,
 		//create Input handler
 		LisaXmlFile xmlInput1(LisaXmlFile::IMPLICIT);
 		LisaXmlFile xmlInput2(LisaXmlFile::IMPLICIT);
+		Lisa_Values V2;
+		bool have_val2 = true;
 		if(!xmlInput1.read(f1) || !(xmlInput1))
 				{
 						cout << "Error : Cannot read source Problem file \"" << f1 << "\"" << endl;
@@ -46,12 +50,12 @@ bool read(const char* f1,
 				}
 		//check types
 		//get Problem
-		if( !(xmlInput1 >> P1))
+		if( !(xmlInput1 >> r_P1))
     {
 						cout << "Error : Cannot read source Problem." << endl;
 						return false;
 				}
-		if( !(xmlInput2 >> P2))
+		if( !(xmlInput2 >> r_P2))
     {
 						cout << "Error : Cannot read target Problem." << endl;
 						return false;
@@ -60,6 +64,31 @@ bool read(const char* f1,
 				{
 						cout << "Error : Cannot read source values." << endl;
 						return false;
+				}
+		if( !(xmlInput2 >> V2))
+				have_val2 = false;
+		cout << "Setting Machine/Job Numbers Property to \"VALUE\"" << endl;
+		m_P1 = r_P1;
+		m_P2 = r_P2;
+
+		
+		m_P1.set_property(M_NUMBER,M_VAL);
+		m_P1.set_property(JOB_NR,J_VAL);
+		m_P2.set_property(M_NUMBER,M_VAL);
+		m_P2.set_property(JOB_NR,J_VAL);
+		
+		m_P1.m_no = V1.get_m();
+		m_P1.n_no = V1.get_n();
+		if(!have_val2)
+				{
+						cout << "No values for target problem. Using size of source problem." << endl;
+						m_P2.m_no = r_P1.m_no;
+						m_P2.n_no = r_P1.n_no;
+				}
+		else
+				{
+						m_P2.m_no = V2.get_m();	
+						m_P2.n_no = V2.get_n();
 				}
 		return true;
 }
@@ -71,19 +100,18 @@ int main(int argc, char** argv)
 				usage(argc,argv);
 				return 0;
 		}
-		Lisa_ProblemType P1, P2;
+		Lisa_ProblemType origP1, origP2, modifP1, modifP2;
 		Lisa_Values V;
-		if(!read(argv[1],argv[2],P1,P2,V))
+		if(!read(argv[1],argv[2],origP1, origP2, modifP1, modifP2,V))
 				{
 						cout << "Failed to read input.\nAborting..." << endl;
 						return 0;
 				}
-		
 		//check reducibility
-		int comp  = Lisa_RedGraph::compare(P1,P2);
+		int comp  = Lisa_RedGraph::compare(origP1,origP2);
 		if((comp != FIRST_TO_SECOND) && (comp != IDENT))
 				{
-						cout << "Error : \"" <<  P1.output_problem() << "\" is not reducible to \"" <<  P2.output_problem() << "\"" << endl;
+						cout << "Error : \"" <<  origP1.output_problem() << "\" is not reducible to \"" <<  origP2.output_problem() << "\"" << endl;
 						return false;
 				}
 		
@@ -94,33 +122,41 @@ int main(int argc, char** argv)
 		const Lisa_ReductionTree* RL[TUPEL_INDEX];
 		
 		
-		Lisa_RedGraph::getReductions(P1,P2,RL);
+		Lisa_RedGraph::getReductions(modifP1,modifP2,RL);
 		
 		Lisa_AtomicTransform::TransformObjects objects;
-		objects.transf_problem = &P1;
-		objects.target_problem = &P2;
+		objects.transf_problem = &modifP1;
+		objects.target_problem = &modifP2;
 		objects.values = &V;
-		
-		cout << "Initial source problem : \"" <<  P1.output_problem() << "\"" << endl;
-		cout << "Initial target problem : \"" <<  P2.output_problem() << "\"" << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << "Initial source problem : \"" <<  origP1.output_problem() << "\"   ->   \""
+							<< modifP1.output_problem() << "\"" << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << "Initial target problem : \"" <<  origP2.output_problem() << "\"   ->   \""
+							<< modifP2.output_problem() << "\"" << endl;
+		cout << "-----------------------------------------" << endl;
 		cout << "Initial values : " << V <<  endl;
-
-
+		cout << "-----------------------------------------" << endl;
+		cout << "-----------------------------------------" << endl;
+		cout << endl << endl;
+		cout << "Looping over all graphs desperate to do transforms." << endl << endl;
+		std::vector<std::pair<int,std::pair<int,int> > > FailedOps;
 		for(int i = 0; i < TUPEL_INDEX; i++)
 				{
+						cout << "**************************************************" << endl;
 						cout << "G = " << name_of_tupelindex[i] << " ... ";
 						if(RL[i] == NULL){
 								cout << "no transform implemented for (" 
-													<< name_of_tupelentry[i][P1.get_property(i)]<< "," 
-													<< name_of_tupelentry[i][P2.get_property(i)]<< ")." << endl;
+													<< name_of_tupelentry[i][modifP1.get_property(i)]<< "," 
+													<< name_of_tupelentry[i][modifP2.get_property(i)]<< ")." << endl;
 								continue;
 						}
 						
 						if( RL[i]->getRoot_ID() ==  RL[i]->getLeaf_ID() ){
-								cout << "identity - nothing to transform for ("
+								cout << "identity - no tree for ("
 													<< name_of_tupelentry[i][RL[i]->getRoot_ID()] << "," 
 													<< name_of_tupelentry[i][RL[i]->getLeaf_ID()] << ")." << endl;
-								continue;
+								//continue;
 						}
 						cout << "printing reduction tree ..." << endl;
 						cout << "---------------------------" << endl;
@@ -133,6 +169,7 @@ int main(int argc, char** argv)
 						//cast const away, because successor stuff is not const method
 						Lisa_Graph *T = (Lisa_Graph*)  RL[i]->getGraph();
 						//identity has NULL Graph -> make the sequence
+						bool have_seq = true;
 						if( T == NULL){
 								p.x = RL[i]->getRoot_ID() + 1;
 								p.y = RL[i]->getLeaf_ID() + 1;
@@ -142,35 +179,77 @@ int main(int argc, char** argv)
 						else
 								{
 										p.x = RL[i]->getRoot_ID() + 1;
+										int n = T-> get_vertices();
 										int source = p.x, target = source;
 										while( (source != (RL[i]->getLeaf_ID() + 1)))
 												{
 														T->init_successor(source);
 														target = T->next_successor(source);
+														if(target == n+1) //no successor found
+																{
+																		cout << "Error : Graph traversal failed. " 
+																							<< 	name_of_tupelentry[i][source - 1] << " doesn't have successor." 
+																							<<  endl << "This is a bug, seriously." << endl;
+																		have_seq = false;
+																		break;
+																}
 														p.x = source;
 														p.y = target;
 														seq.append(p);
 														source = target;
 												}
+										if(!have_seq)
+												continue;
 								}
+						//apply transform
+						cout << "Running forward transform with Sequence : {";
 						//output sequence
-						cout << "Transform sequence : {";
 						seq.reset();
 						do
 								{
 										Lisa_Pair p = seq.get();
-										cout << " [" << name_of_tupelentry[i][p.x - 1] << "," << name_of_tupelentry[i][p.y - 1] << "]";
+										cout << " [" << name_of_tupelentry[i][p.x - 1] << "," 
+															<< name_of_tupelentry[i][p.y - 1] << "]";
 								}while(seq.next());
-						cout << " }" << endl;
+						
+						cout << " }  ... ";
 						seq.reset();
-						//apply transform
-						RL[i]->forwardTransform(seq,objects);
+						if(RL[i]->forwardTransform(seq,objects))
+								cout << "okay, went well" << endl;
+						else{
+								cout << "failed." << endl;
+								FailedOps.push_back(std::pair<int,std::pair<int,int> > (i,std::pair<int,int>( RL[i]->getRoot_ID(),  RL[i]->getLeaf_ID())));
+						}
 				}
-		
-		cout << "Final source problem : \"" <<  P1.output_problem() << "\"" << endl;
-		cout << "Final target problem : \"" <<  P2.output_problem() << "\"" << endl;
-		cout << "Final values : " << V <<  endl;
+		cout << endl << endl;
+		if(!FailedOps.empty())
+				{
+						cout << "The following transforms failed :"<< endl;
+						std::vector<std::pair<int,std::pair<int,int> > >::iterator it;
+						for(it = FailedOps.begin(); it != FailedOps.end(); ++it){
+								cout << "[ \"" << name_of_tupelentry[it->first][it->second.first] << "\" (";
+								cout << it->second.first << ") ]  -> ";
+								cout << "[ \"" << name_of_tupelentry[it->first][it->second.second] << "\" (";
+								cout << it->second.second << ") ]  in  ";
+								cout << "\"" << name_of_tupelindex[it->first] << "\" (";
+								cout << it->first << ")" << endl;
+						}
+				}
+		cout << endl << endl;
 
+		cout << "Resetting Machine/Job Numbers Property to original state" << endl << endl;
+		modifP1.set_property(M_NUMBER,origP1.get_property(M_NUMBER));
+		modifP1.set_property(JOB_NR,origP1.get_property(JOB_NR));
+		modifP2.set_property(M_NUMBER,origP2.get_property(M_NUMBER));
+		modifP2.set_property(JOB_NR,origP2.get_property(JOB_NR));
+
+		cout << "Here is what we finally got from the transforms :" << endl << endl;
+		cout << "Final source problem : \"" <<  modifP1.output_problem() << "\"" << endl;
+		cout << "Final target problem : \"" <<  modifP2.output_problem() << "\"" << endl;
+		cout << "Final values : " << V <<  endl;
+		
+
+		
 		return 1;
 }
 		
