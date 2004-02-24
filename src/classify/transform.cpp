d476cc2f5c8146d6cc6be3c87e1f4ef8cba2026d
tusch <tusch>
2004-02-24 15:56:28 +0000

    *** empty log message ***

--- src/classify/transform.cpp
+++ src/classify/transform.cpp
@@ -5,68 +5,98 @@
 
 
 // NOTE: The current implementation should just give a hint of how to do the transform.
-//       It is neither complete nor claimed to be correct.
+//       The actual transform is missing.
+//       This example demonstrates how to use one Atomic_Transform for both directions.
+//       The Identity transform is invoked for any pair (s,t) for every graph g.
+//       The interesting case is when the sizes of the two involved problem instances differ.
+
 bool 
 Identity_AtomicTransform::transform(int g ,int s,int t,
 																																				TransformObjects& to,
 																																				TransformDir d)
 {
+		//check for correct transform
 		if(s != t)
 				return false;
-		if(g == M_NUMBER && s == M_VAL) //pumping machines
+		if(g == M_NUMBER && s == M_VAL) //process number of machines
 				{
+						//problems has fixed aize ?
 						if( to.transf_problem->get_property(M_NUMBER) != M_VAL ||
 										to.target_problem->get_property(M_NUMBER) != M_VAL)
 								return false;
-						//forward transform
+						//perform forward transform
 						if(d == FORWARD)
 								{
-										if(to.transf_problem->m_no > to.target_problem->m_no)
+										//check special cases of problem sizes
+										if(to.transf_problem->m_no > to.target_problem->m_no) //cannot decrease
 												return false;
-										if(to.transf_problem->m_no == to.target_problem->m_no)
+										if(to.transf_problem->m_no == to.target_problem->m_no) //nothing to do
 												return true;
+										//check validity of target problem
 										if(!(to.values-> valid) || (((int)to.transf_problem->m_no) != to.values->get_m()))
 												return false;
-										// we have a real pump-up to perform
-										//alter values, do not touch schedule
-										//processing times
-										unsigned m_new = to.target_problem->m_no;
-										unsigned m_old =  to.values->get_m();
-										unsigned n =  to.values->get_n();
-										//i hope init does the the job ???? - MO may be destroyed ???
-										to.values->init(n,m_new);
-										if(to.values->PT)
-												{
-														for(unsigned i = 0; i < n; i++)
-																for(unsigned j = m_old ; j < m_new; j++)
-																		to.values->PT[i][j] = 0;
-												}
-										to.values->PT_to_SIJ();
+										//okay here comes the transform
+										/* +++++++++++++++++++++++++++++
+											*
+											*  put transform code here
+											*
+											* +++++++++++++++++++++++++++++ 
+											*/
 										return true;
 								}
 						//backwards transform
-						if(to.transf_problem->m_no < to.target_problem->m_no)
+						//check special cases of problem sizes
+						if(to.transf_problem->m_no < to.target_problem->m_no)//cannot increase
 								return false;
-						if(to.transf_problem->m_no == to.target_problem->m_no)
-								return true;	
+						if(to.transf_problem->m_no == to.target_problem->m_no) //nothing to do
+								return true;
+						//ckeck validity
 						if(!(to.values-> valid) || (((int)to.transf_problem->m_no) != to.values->get_m()))
 								return false;
-						// reinit should remove all machines from members
-						to.values->init(to.values->get_n(),to.target_problem->m_no);
-						// we have a real pump-down to perform
-						// alter objects
-						to.transf_problem->m_no = to.target_problem->m_no;
-						// todo : alter schedule
+						/* +++++++++++++++++++++++++++++
+							*
+							*  put transform code here
+							*
+							* +++++++++++++++++++++++++++++ 
+							*/
 						return true;
 				}
-		if(g == JOB_NR && s == J_VAL) //pumping jobs
+		if(g == JOB_NR && s == J_VAL) //process number of jobs
 				{
-						//not implemented - the above is just an example of how to transform
+						/* +++++++++++++++++++++++++++++
+							*
+							*  put transform code here
+							*
+							* +++++++++++++++++++++++++++++ 
+							*/
 						return false;
 				}
 		return true;
 }
 
+//implementation of SumCi -> SumWiCi
+
+bool SumWiCi_AtomicTransform::transform(int g ,int s,int t,
+																																								TransformObjects& to,
+																																								TransformDir dir){
+		//check signature
+		if( (g != OBJECTIVE) || (s != SUM_CI) || (t != SUM_WICI))
+				return false;
+		switch(dir)
+				{
+				case FORWARD: //forward transform
+						to.values->make_WI(); //create weights
+						to.values->WI->fill(1.0); //unit fill
+						to.transf_problem->set_property(g,t); //set problem signature
+						break;
+				case BACKWARDS:
+						to.values->no_WI(); // destroy weights
+						to.transf_problem->set_property(g,s); //set problem signature
+						break;
+				}
+		//that's it we were successful
+		return true;
+}
 
 
 
--- src/classify/transform.hpp
+++ src/classify/transform.hpp
@@ -17,11 +17,33 @@ class Identity_AtomicTransform : public virtual Lisa_AtomicTransform {
 			 TransformDir dir);
 };
 
-
 static Lisa_AtomicTransform* IdentityAtomic = new Identity_AtomicTransform();
 static Lisa_Transform* IdentityTransform = new Lisa_Transform(IdentityAtomic,IdentityAtomic,0);
 
 //@}
 
+
+//another transform
+
+//@{
+/** Class performing objective function transform from SumCi to SumWiCi.
+ *  
+ *  @author Jan Tusch
+ *  @version 2.3pre3
+ */
+
+/// Atomic Transform class for transforming SumCi to SumWiCi.
+class SumWiCi_AtomicTransform : public virtual Lisa_AtomicTransform {
+ public:
+  /// actual transform function
+  virtual bool transform(int g ,int s,int t,
+			 TransformObjects& to,
+			 TransformDir dir);
+};
+
+static Lisa_AtomicTransform* SumWiCi_Atomic = new SumWiCi_AtomicTransform();
+static Lisa_Transform* SumWiCi_Transform = new Lisa_Transform(SumWiCi_Atomic,SumWiCi_Atomic,0);
+
+//@}
 
 #endif
