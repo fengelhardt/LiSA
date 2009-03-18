47bda7c351ba127bb7112726fa3592d7bd82549d
engelhardt <engelhardt>
2009-03-18 16:32:31 +0000

    Added a windows batch file, that needs no more configuration. It updates LISAHOME according to the current working directory and starts LiSA from there.

--- src/main/lisa.bat
+++ src/main/lisa.bat
@@ -5,7 +5,7 @@ rem written by Frank Engelhardt
 
 rem setting the LiSA home path
 rem these nice commands put the current directory into LISAHOME in 8.3 short format
-for %%x in (%cd%) do set LISAHOME=%%~sx
+for %%x in ("%cd%") do set LISAHOME=%%~sx
 set LISAHOME=%LISAHOME%/
 
 rem setting the LiSA config path
